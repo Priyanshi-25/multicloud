d84d4052811eddcab5218cb4930243dc7b4e0cf0
Priyanshi-25 <65086465+Priyanshi-25@users.noreply.github.com>
2020-06-13 21:40:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <pre>
-<img src="d3p89w4hjki69l.cloudfront.net/image" height="100px" width = "100px"> 
+<img src="d3p89w4hjki69l.cloudfront.net/image/cloud-comput.jpg" height="200px" width = "100px"> 
 <?php
 print "cloud task \n";
 print  `ifconfig`;
